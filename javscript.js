14e85eae7909ca560ce746dbce2d2df7a2aedbba
Abhay Kumar <shindheabhay@gmail.com>
2023-08-15 18:25:50 +0530

    modified a line of program

--- javscript.js
+++ javscript.js
@@ -4,25 +4,26 @@ const buttons = document.querySelectorAll('button');
 
 let string = "";
 let arr = Array.from(buttons);
-arr.forEach(button => {
-    button.addEventListener("click", (e)=>{
-           if(e.target.innerHTML == '='){
-            string=eval(string);
-            input.value = string;
-           }
 
-           else if(e.target.innerHTML == 'AC'){
+arr.forEach(button => {
+    button.addEventListener("click", (e) => {
+        if (e.target.innerHTML === '=') {
+            try {
+                string = (new Function('return ' + string))() ||"";
+                input.value = string;
+            } catch (error) {
+                // Handle the error gracefully if the expression is invalid
+                input.value = 'Error';
+            }
+        } else if (e.target.innerHTML === 'AC') {
             string = "";
             input.value = string;
-           }
-           else if(e.target.innerHTML=='DEL'){
+        } else if (e.target.innerHTML === 'DEL') {
             string = string.slice(0, -1);
             input.value = string;
-           }
-           else{
+        } else {
             string += e.target.innerHTML;
             input.value = string;
-           }
-
+        }
     })
 })
